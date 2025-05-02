9ded1dbeba19ae7d36e3ef68a9c93b554d2150c5
pakash2003pramanick <prakashpramanickjsr1717@gmail.com>
2025-05-02 18:53:40 +0530

    updated bash file

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define image and container names
-IMAGE_NAME="plagarism-checker"
-CONTAINER_NAME="plagarism-checker-container"
+IMAGE_NAME="plagiarism-checker"
+CONTAINER_NAME="plagiarism-checker-container"
 
 # Stop and remove any existing container with the same name
 docker rm -f $CONTAINER_NAME 2>/dev/null
@@ -13,4 +13,4 @@ docker build -t $IMAGE_NAME .
 
 # Run the Docker container with environment variables from .env file
 echo "Running Docker container..."
-docker run --env-file .env --name $CONTAINER_NAME -p 80:80 $IMAGE_NAME
+docker run -d --env-file .env -p 80:80 --name $CONTAINER_NAME $IMAGE_NAME
